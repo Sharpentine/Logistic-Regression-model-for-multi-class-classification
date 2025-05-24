398f1316b3de9c729b979b495ce3221b1090cd97
Fay <94161175+Sharpentine@users.noreply.github.com>
2025-05-25 01:46:26 +0530

    Add files via upload

---
+++ streamlit_digit_classifier.py
@@ -0,0 +1,23 @@
+
+import streamlit as st
+import numpy as np
+import joblib
+from PIL import Image, ImageOps
+
+st.title("Digit Classifier (Logistic Regression)")
+st.write("Upload an image of a digit (0–9)")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg", "jpeg"])
+
+if uploaded_file is not None:
+    model = joblib.load("logistic_digit_model.pkl")
+
+    image = Image.open(uploaded_file).convert("L")  
+    image = ImageOps.invert(image)  
+    image = image.resize((28, 28))
+    img_array = np.array(image).astype("float32") / 255.0
+    img_flat = img_array.reshape(1, -1)
+
+    prediction = model.predict(img_flat)[0]
+    st.image(image, caption="Processed Image", width=150)
+    st.write(f"### Predicted Digit: {prediction}")
