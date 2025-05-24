36a34a25766d2c00a0987cb4a1839f52203dbcf7
Fay <94161175+Sharpentine@users.noreply.github.com>
2025-05-25 01:54:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,14 @@ uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg", "jpeg
 if uploaded_file is not None:
     model = joblib.load("logistic_digit_model.pkl")
 
+    import cv2
     image = Image.open(uploaded_file).convert("L")  
     image = ImageOps.invert(image)  
     image = image.resize((28, 28))
+    
+    img_array = np.array(image).astype("uint8")
+    _, img_array = cv2.threshold(img_array, 128, 255, cv2.THRESH_BINARY)
+    
     img_array = np.array(image).astype("float32") / 255.0
     img_flat = img_array.reshape(1, -1)
 
